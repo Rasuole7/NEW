f36470f599db6a30cd12892b9d65798178e58b64
Rasuole7 <widercreativeperspectives@gmail.com>
2023-04-24 22:01:04 +0300

    Initial commit

---
+++ src/main/java/KayakTest.java
@@ -0,0 +1,39 @@
+import org.checkerframework.checker.units.qual.C;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class KayakTest extends KayakBase {
+    private KayakHome kayakHome;     // kuriam prieiga prie metodu
+
+    public KayakTest() { // tuscias konstruktorius be jokiu argumentu
+        super(driver);
+    }
+
+
+    @BeforeClass
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:/Users/XPS 15/Downloads/chromedriver_win32/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        KayakBase.driver = new ChromeDriver(options);
+        KayakBase.driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void KayakStayTest(){
+      KayakBase.goTo();
+      KayakHome.stays();
+    }
+
+   @AfterClass
+    public static void tearDown(){
+    driver.quit();
+    }
+
+}
+
