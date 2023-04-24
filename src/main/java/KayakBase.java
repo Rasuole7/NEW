eaf1c76f9b6832879c4e3cebf5b6c5996e684cd7
Rasuole7 <widercreativeperspectives@gmail.com>
2023-04-24 21:59:47 +0300

    Initial commit

--- src/main/java/KayakBase.java
+++ src/main/java/KayakBase.java
@@ -6,9 +6,12 @@ public class KayakBase {
     protected static WebDriver driver; // kitose klasese paveldesime sita driveri
 
     public KayakBase (WebDriver driver){ // kuriame konstruktoriu su savybemis
-        KayakBase.driver = driver; // priskiriame savybe prie naujos reiksmes; nenaudojame this.driver, kad neraudonuotu
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Duration - objektas, ofSeconds - metodai
+        KayakBase.driver = driver; // priskiriame savybe prie naujos reiksmes; nenaudojame this.driver, kad neraudonuotu, parasem klases pavadinimas
+        //driver.manage().window().maximize();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Duration - objektas, ofSeconds - metodai
     }
 
+    public static void goTo() {
+        driver.get("https://www.kayak.com/");
+    }
 }
--- src/main/java/KayakHome.java
+++ src/main/java/KayakHome.java
@@ -1,15 +1,33 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class KayakHome extends KayakBase {
 
-    public KayakHome (WebDriver driver){ //konstruktorius
-     super (driver);
+    private static final By stayButton = By.cssSelector("a[aria-label='Search for hotels']");
+
+    public KayakHome(WebDriver driver) { //konstruktorius
+        super(driver);
     }
 
 
-    public void goTo(){
+
+
+    public static void stays() {
+        WebDriverWait stayButtonWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        try {
+            WebElement stay = stayButtonWait.until(ExpectedConditions.visibilityOfElementLocated(stayButton));
+            stay.click();
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println("Stay button is not displayed"); // jei elementas stay nerodomas, isprintins sita zinute
+        }
 
     }
-
 
 }
